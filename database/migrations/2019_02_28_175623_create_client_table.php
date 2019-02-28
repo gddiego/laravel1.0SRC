0f3fe15ab47a9a95a25c3042f584f13aa4f57a64
Diego <Diego@Mendes>
2019-02-28 14:18:39 -0400

    projeto inicialize laraevl

---
+++ database/migrations/2019_02_28_175623_create_client_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('email', 100);
+            $table->string('endereco');
+            $table->char('cpf', 11);
+            $table->string('telefone');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client');
+        Schema::connection('foo')->create('client', function (Blueprint $table){
+            $table->increments('id');
+        });
+    }
+}
